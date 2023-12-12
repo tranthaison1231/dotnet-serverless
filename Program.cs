513aea5c42b9faacccac7663a66e0407edce244a
son.tran <son.tran@team.enouvo.com>
2023-12-12 14:42:28 +0700

    feat(master): update file csproj

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
